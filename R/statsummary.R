96e24d9216c699f3eec1a8e6d0121da6e7b08543
Haoyu Sheng <haoyusheng@Haoyus-MacBook-Pro.local>
2017-02-18 16:35:25 -0500

    Minor cleanup

--- R/statsummary.R
+++ R/statsummary.R
@@ -8,23 +8,23 @@
 #' @export
 
 statsummary<- function(type) {
-  data <- readHonor(DataSummary,chineseLastName)
-  data$origin<- "Chinese"
-  temp <- readHonor(DataSummary,cantoneseLastName)
-  temp$origin<- "Cantonese"
-  data <- rbind (data, temp)
-  temp2 <- readHonor(DataSummary,japaneseLastName)
-  temp2$origin<-"Japanese"
-  data <- rbind (data, temp2)
+  data = readHonor(DataSummary, totalVeri)
   if (type == "summary by count") {
-  ggplot(data, aes (year, fill = honor))+geom_bar()+labs(y = "Count", x = "Year", title = "Number of Students of Asian Descent from 2002 to 2016")
-
+    ggplot(data, aes (year, fill = honor))+geom_bar()+labs(y = "Count", x = "Year", title = "Number of Students of Asian Descent from 2002 to 2016")
   }
   else if (type == "summary by percentage") {
-  ggplot(data, aes (year, fill = honor))+geom_bar(aes(fill = honor), position = "fill")+geom_hline(yintercept = 0.02)+geom_hline(yintercept = 0.15)+geom_hline(yintercept = 0.35)+labs(y = "Percent", x = "Year", title = "Numbers of students of Asian descent from 2002 to 2016(compared to average Latin Honor)")}
+    ggplot(data, aes (year, fill = honor))+geom_bar(aes(fill = honor), position = "fill")+geom_hline(yintercept = 0.02)+geom_hline(yintercept = 0.15)+geom_hline(yintercept = 0.35)+labs(y = "Percent", x = "Year", title = "Numbers of students of Asian descent from 2002 to 2016(compared to average Latin Honor)")}
   else if (type == "summary by origin") {
+    #sort out the data by origin
+    data = readHonor(DataSummary,chineseLastName)
+    data$origin<- "Chinese"
+    temp <- readHonor(DataSummary,cantoneseLastName)
+    temp$origin<- "Cantonese"
+    data <- rbind (data, temp)
+    temp2 <- readHonor(DataSummary,japaneseLastName)
+    temp2$origin<-"Japanese"
+    data <- rbind (data, temp2)
     ggplot(data, aes(origin, fill =  honor))+ geom_bar()+labs(y = "Count", x = "Origin", title = "Number of students of Asian descent from 2002 to 2016 grouped by origin")
-
   }
 
   else if (type == "histogram"){
